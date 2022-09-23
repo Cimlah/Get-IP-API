08b8a0104791d6c7f97d1a9bbdc8faf0bb7e5d3c
Michał Stolarczyk <28655852+Cimlah@users.noreply.github.com>
2022-09-23 22:09:17 +0200

    Created a `/` route for showing client's IP address

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', function (req, res) {
+    let ip = req.ip
+
+    res.set('Content-Type', 'application/json')
+    res.send({
+        'your-ip': ip
+    })
+})
+
+app.listen(port, function (err) {
+    if(err) {console.log(err)}
+
+    console.log('Running on http://localhost:' + port)
+})
