d0b319c2e23351d63d7038e432fc2bb003f7be87
Michał Stolarczyk <28655852+Cimlah@users.noreply.github.com>
2022-09-24 12:12:12 +0200

    Added Redis to `/` route, so when client connects, it counts how many times the client connected from specific IP address

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', function (req, res) {
+const redis = require('redis')
+const redisClient = redis.createClient()
+redisClient.on('error', (err) => {console.log('Redis ' + err)})
+const redisConnect = async () => {redisClient.connect()}; redisConnect();
+
+app.get('/', async function (req, res) {
     let ip = req.headers['x-forwarded-for'] || req.ip
 
+    if((await redisClient.keys(ip)).length == 0) {
+        await redisClient.hSet(ip, 'value', ip)
+        await redisClient.hSet(ip, 'times-hit', 1)
+    }
+    else {
+        redisClient.hIncrBy(ip, 'times-hit', 1)
+    }
+    
     res.set('Content-Type', 'application/json')
     res.send({
-        'your-ip': ip
+        'your-ip': ip,
+        'times-hit': await redisClient.hGet(ip, 'times-hit')
     })
 })
 
